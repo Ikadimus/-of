e1a9f65b8f07a98283451b99835945cef97cb11f
Ikadimus <evaldo.design@hotmail.com>
2025-12-06 12:47:41 -0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './index.tsx',
@@ -17,6 +18,13 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+  ],
   mode: 'production',
 };
